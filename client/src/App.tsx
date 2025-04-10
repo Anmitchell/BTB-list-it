3ace0f51f7aef67142f0ac2a6ba62ac5307b1518
Anmitchell <avarynmitchell@gmail.com>
2025-04-09 20:47:38 -0400

    Setup up development proxy for frontend to connect to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
-import "./App.css";
+import './App.css';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch('/api/todos');
+      const todos = await res.json();
+
+      setMessage(todos.message);
+    }
+    getTodos();
+  }, []);
+
   return (
     <div>
       <h1>Hello World</h1>
+      {message && <p>{message}</p>}
     </div>
   );
 }
-
